220b154ccc8a9a3557d8da42b5c48e67a2ccf51b
YuriSalesdeOliveira <yuri_oli@hotmail.com>
2022-02-04 10:28:23 -0300

    Style: responsive layout

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -17,4 +17,21 @@
 
     }
 
+    let show_menu_button = document.querySelector('#show_menu')
+    let menu = document.querySelector('.main_header_nav')
+
+    show_menu_button.addEventListener('click', () => {
+
+        menu.style.display = 'flex'
+
+    })
+
+    let hide_menu_button = document.querySelector('#hide_menu')
+
+    hide_menu_button.addEventListener('click', () => {
+
+        menu.style.display = 'none'
+
+    })
+
 })()
