d06c35bbf447f9dc453ad29c0af4836211645e72
YuriSalesdeOliveira <yuri_oli@hotmail.com>
2022-02-06 00:53:58 -0300

    Feature: adding router posts

--- public/index.php
+++ public/index.php
@@ -1,6 +1,7 @@
 <?php
 
 use Slim\Factory\AppFactory;
+use Source\http\Controllers\Post;
 use Source\http\Controllers\Web;
 
 require_once(dirname(__DIR__) . '/vendor/autoload.php');
@@ -11,5 +12,16 @@ $app->setBasePath('/public');
 
 $app->get('/', [Web::class, 'home'])->setName('web.home');
 $app->get('/{page}', [Web::class, 'home'])->setName('web.home.arg');
+
+$app->get('/postagens', [Post::class, 'posts'])->setName('post.posts');
+$app->get('/postagens/{post}', [Post::class, 'show'])->setName('post.show');
+
+$app->get('/postagens/criar', [Post::class, 'create'])->setName('post.create');
+$app->post('/postagens', [Post::class, 'store'])->setName('post.store');
+
+$app->get('/postagens/{post}/editar', [Post::class, 'edit'])->setName('post.edit');
+$app->put('/postagens/{post}', [Post::class, 'update'])->setName('post.update');
+
+$app->delete('/postagens/{post}', [Post::class, 'destroy'])->setName('post.destroy');
 
 $app->run();
