1c40b038947fa226f0b589cb3236445c34acb390
YuriSalesdeOliveira <yuri_oli@hotmail.com>
2022-02-06 19:44:54 -0300

    Removing public from url

--- public/index.php
+++ public/index.php
@@ -7,8 +7,6 @@ require_once(dirname(__DIR__) . '/vendor/autoload.php');
 
 $app = AppFactory::create();
 
-$app->setBasePath('/public');
-
 $app->get('/', [Web::class, 'home'])->setName('web.home');
 $app->get('/{page}', [Web::class, 'home'])->setName('web.home.arg');
 
