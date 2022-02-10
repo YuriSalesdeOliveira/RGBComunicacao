3a0707860a43033b09ffd546568b1f5830070126
YuriSalesdeOliveira <yuri_oli@hotmail.com>
2022-02-10 00:18:48 -0300

    Feature: adding class description

---
+++ source/Classes/Description.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Source\Classes;
+
+use Exception;
+
+class Description
+{
+    protected string $value;
+
+    public function __construct(string $value)
+    {
+        $this->value = $value;
+    }
+
+    public static function parse(string $value): static
+    {
+        if (empty($value)) { throw new Exception('O valor de description não pode ser vazio.'); }
+
+        $description = new static($value);
+
+        return $description;
+    }
+
+    public static function tryParse(string $value) {}
+
+    public function __toString()
+    {
+        return $this->value;
+    }
+}
