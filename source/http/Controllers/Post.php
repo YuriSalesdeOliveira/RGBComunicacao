bbf76bf7c60433dbf310f4e13896e8be191cd4a6
YuriSalesdeOliveira <yuri_oli@hotmail.com>
2022-02-10 20:00:13 -0300

    Feature: adding domain  folder

--- source/http/Controllers/Post.php
+++ source/http/Controllers/Post.php
@@ -2,6 +2,9 @@
 
 namespace Source\http\Controllers;
 
+use Source\Domain\Classes\Photo;
+use Source\Models\Post as ModelsPost;
+use Source\Domain\Classes\Description;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
@@ -9,12 +12,14 @@ class Post extends Controller
 {
     public function posts(Request $request, Response $response)
     {
-        // view
+        
     }
 
     public function show(Request $request, Response $response)
     {
-        // view "um post"
+        $response->getBody()->write('Post');
+
+        return $response;
     }
 
     public function create(Request $request, Response $response)
@@ -24,7 +29,10 @@ class Post extends Controller
 
     public function store(Request $request, Response $response)
     {
-
+        $post = new ModelsPost();
+        $post->photo = Photo::parse([]);
+        $post->description = Description::parse('Nome do Álbum Lorem Ipsum Dolor Amed');
+        $post->save();
     }
 
     public function edit(Request $request, Response $response)
